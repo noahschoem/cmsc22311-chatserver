5dad973851281ea3974118a4c3b64ad95bb53ddc
Noah Schoem <nschoem@gmail.com>
2015-05-27 14:34:56 -0500

    Reverted to the previous way and got join messages broadcast to everyone.

--- src/Chat.hs
+++ src/Chat.hs
@@ -1,19 +1,16 @@
 -- | CS240h Lab 2 Chat Server
--- This code is taken heavily from the Haskell Wiki's guide on how to build a chat server
--- (https://wiki.haskell.org/Implement_a_chat_server)
--- and rewritten and modified to fit the requirements of the project.
+{- This code is taken heavily from the Haskell Wiki's guide on how to build a chat server
+   (https://wiki.haskell.org/Implement_a_chat_server)
+   and rewritten and modified to fit the requirements of the project. -}
 module Chat (chat) where
 import Network.Socket
 import System.IO
 import Control.Concurrent
 import Control.Concurrent.Chan
-import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.Fix (fix)
 
-type Message = (String,Int) -- message body
-type State = [Chan Message]
-type StateVar = MVar State
+type Message = (String,Int) -- (message body,user id)
 
 -- | Chat server entry point.
 chat :: PortNumber -> IO ()
@@ -25,33 +22,26 @@ chat port = do
   -- listen on TCP port 4242
   bindSocket sock (SockAddrInet port iNADDR_ANY)
   listen sock 2
-  state <- newMVar []
-  mainLoop sock state 1
+  chan <- newChan
+  mainLoop sock chan 1
   
-mainLoop :: Socket -> StateVar -> Int -> IO ()
-mainLoop sock state n = do   
+mainLoop :: Socket -> Chan Message -> Int -> IO ()
+mainLoop sock chan n = do   
   conn <- accept sock
-  forkIO (runConn conn state n)
-  mainLoop sock state (n+1)
+  forkIO (runConn conn chan n)
+  mainLoop sock chan (n+1)
 
-runConn :: (Socket, SockAddr) -> StateVar -> Int -> IO ()
-runConn (sock, _) state n = do
+runConn :: (Socket, SockAddr) -> Chan Message -> Int -> IO ()
+runConn (sock, _) chan n = do
   hdl <- socketToHandle sock ReadWriteMode
   hSetBuffering hdl NoBuffering
-  channels <- takeMVar state
-  chan <- newChan
-  mapM (\c -> writeChan c (show n ++ " has joined.",n)) $ chan:channels
-  putMVar state $ chan:channels
+  chan' <- dupChan chan
+  writeChan chan (show n ++ " has joined.",0)
   forkIO $ fix $ \loop -> do
-    line <- liftM init (hGetLine hdl)
-    channels' <- takeMVar state
-    mapM (\c-> writeChan c (line,n)) $ filter (/= chan) channels'
-    putMVar state channels'
+    (line,user) <- readChan chan'
+    when(user /= n) $ hPutStrLn hdl line
     loop
   fix $ \loop -> do
-    channels' <- takeMVar state
-    let channel = head $ filter (== chan) channels'
-    (line,user) <- readChan channel
-    putMVar state channels'
-    hPutStrLn hdl $ show user ++ ": " ++ line
+    line <- liftM init (hGetLine hdl)
+    writeChan chan' (show n ++ ": " ++ line,n)
     loop
