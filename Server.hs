d3d20a74feb38257b442fd530b1f6b90da499300
Noah Schoem <nschoem@gmail.com>
2015-05-27 18:41:00 -0500

    Stripped down imports.

--- Server.hs
+++ Server.hs
@@ -3,19 +3,20 @@
 module Main (main) where
 
 import Chat
-import System.Environment
+import System.Environment (lookupEnv)
 
 -- | Run our chat server.
 main :: IO ()
 main = do
-  -- using lookupEnv instead of getEnv for maximum portability
   port <- lookupEnv "CHAT_SERVER_PORT"
-  print port
   case port of
        Nothing -> defaultChat
        Just "" -> defaultChat
        Just x  -> chat $ read x
-       
+         
+-- | default to using port 4242 if the 
+--   environment variable CHAT_SERVER_PORT is undefined
+defaultChat :: IO ()
 defaultChat = do
   putStrLn "CHAT_SERVER_PORT environment variable undefined.  Defaulting to Port 4242."
   chat 4242
--- src/Chat.hs
+++ src/Chat.hs
@@ -1,21 +1,24 @@
 -- | CS240h Lab 2 Chat Server
-{- This code is taken heavily from the Haskell Wiki's guide on how to build a chat server
+{- This code is taken heavily from the Haskell Wiki's guide 
+   on how to build a chat server
    (https://wiki.haskell.org/Implement_a_chat_server)
    and rewritten and modified to fit the requirements of the project. -}
 module Chat (chat) where
-import Network
-import System.IO
-import Control.Concurrent
-import Control.Monad
+import Network    (Socket, PortID (PortNumber), 
+                    withSocketsDo, listenOn, accept)
+import System.IO  (BufferMode (NoBuffering), Handle,
+                   hSetBuffering, hPutStrLn, hGetLine, hClose)
+import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.Chan
+import Control.Monad     (when)
 import Control.Monad.Fix (fix)
-import Control.Exception
+import Control.Exception (SomeException (..), handle)
 
 type Message = (String,Int) -- (message body,user id)
 
 -- | Chat server entry point.
 chat :: Int -> IO ()
 chat port = withSocketsDo $ do
-  -- create socket
   sock <- listenOn $ PortNumber $ fromIntegral port
   chan <- newChan
   mainLoop sock chan 1
@@ -36,7 +39,7 @@ runConn hdl chan n = do
   writeChan chan ("Welcome " ++ show n ++ " to the server!",0)
   thread <- forkIO $ fix $ \loop -> do
     (line,user) <- readChan chan'
-    when(user /= n) $ hPutStrLn hdl line
+    when (user /= n) $ hPutStrLn hdl line
     loop
   handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
     line <- hGetLine hdl
